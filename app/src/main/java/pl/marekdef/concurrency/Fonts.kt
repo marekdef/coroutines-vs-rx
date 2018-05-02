713a907ae21fe53e96d3dcfba4bedd78b99a0fd8
Marek Defecinski <marekdef@gmail.com>
2018-05-02 11:15:58 +0200

    Changed observable to single in retorfit Updated layout so we have max 25 text updated libraries to latest versions

--- app/src/main/java/pl/marekdef/concurrency/Fonts.kt
+++ app/src/main/java/pl/marekdef/concurrency/Fonts.kt
@@ -2,13 +2,14 @@ package pl.marekdef.concurrency
 
 import android.graphics.Bitmap
 import io.reactivex.Observable
+import io.reactivex.Single
 import kotlinx.coroutines.experimental.Deferred
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface Fonts {
     @GET("/font/{font}")
-    fun font(@Path("font") font: Char) : Observable<Bitmap>
+    fun fontSingle(@Path("font") font: Char) : Single<Bitmap>
 
 
     @GET("/font/{font}")
--- app/src/main/java/pl/marekdef/concurrency/FontsActivity.kt
+++ app/src/main/java/pl/marekdef/concurrency/FontsActivity.kt
@@ -2,34 +2,43 @@ package pl.marekdef.concurrency
 
 import android.graphics.Bitmap
 import android.os.Bundle
-import android.os.Environment
+import android.os.StrictMode
 import android.support.v7.app.AppCompatActivity
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
+import android.view.View
 import android.widget.ImageView
+import android.widget.LinearLayout
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.experimental.CoroutineCallAdapterFactory
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import com.jakewharton.rxbinding2.widget.RxTextView
+import hu.akarnokd.rxjava2.debug.RxJavaAssemblyTracking
 import io.reactivex.Observable
+import io.reactivex.Single
+import io.reactivex.SingleObserver
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.functions.BiFunction
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fonts_main.*
 import kotlinx.coroutines.experimental.Deferred
 import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.launch
-import okhttp3.Cache
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
+import kotlin.system.measureNanoTime
 
 class FontsActivity : AppCompatActivity() {
     companion object {
         val TAG = FontsActivity::class.java.simpleName
 
         val okhttp = OkHttpClient.Builder()
-                .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
-                .cache(Cache(Environment.getDownloadCacheDirectory(), 512))
+                .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BASIC))
+//                .cache(Cache(Environment.getDownloadCacheDirectory(), 1_024 * 1_024 * 5))
                 .build();
 
         val fonts = Retrofit.Builder()
@@ -49,14 +58,64 @@ class FontsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        RxJavaAssemblyTracking.enable()
         setContentView(R.layout.fonts_main)
+
+        for (i in 1..25) {
+            linearLayoutRx.addView(ImageView(this))
+            linearLayoutCoroutines.addView(ImageView(this))
+        }
+
+        StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder().detectAll().penaltyDeath().build())
     }
 
 
     override fun onResume() {
         super.onResume()
 
+        setupRx()
+
+//        setupCoroutine()
+    }
+
+    private fun setupRx() {
+        compositeTextChange.add(RxTextView.afterTextChangeEvents(editTextName)
+                .debounce(500, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .map { it.editable().toString() }
+                .doOnNext {
+                    resetViews(linearLayoutRx, it.length)
+                }
+                .doOnNext {
+                    compositeFonts.clear()
+                }
+                .filter { it.isNotEmpty() }
+                .flatMap {
+                    Observable.fromIterable(it.toCharArray().asIterable())
+                            .map { fonts.fontSingle(it).subscribeOn(Schedulers.io()) }
+                            .concatMapSingle { it }
+                            .zipWith(Observable.range(0, 25), BiFunction {b: Bitmap, idx: Int -> Pair(idx, b)})
+//                            .zipWith(Observable.range(0, 25), BiFunction { c: Char, idx: Int -> Pair(idx, c) })
+//                            .flatMapSingle { (index, char) ->
+//                                fonts.fontSingle(char)
+//                                        .subscribeOn(Schedulers.io())
+//                                        .map { bitmap -> index to bitmap }
+//                            }
+                }
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    val imageView = linearLayoutRx.getChildAt(it.first) as ImageView
+
+                    imageView.setImageBitmap(it.second)
+                    imageView.visibility = View.VISIBLE
+                })
+    }
+
+    private fun setupCoroutine() {
         editTextName.addTextChangedListener(object : TextWatcher {
+            var runnable: Runnable? = null
+
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
 
             }
@@ -66,52 +125,59 @@ class FontsActivity : AppCompatActivity() {
 
             override fun afterTextChanged(s: Editable) {
                 val text = s.toString()
-                if (text.isEmpty())
-                    return
 
-                linearLayoutCoroutines.removeAllViews()
-                coroutines.forEach { it.cancel() }
-
-                text.toCharArray().forEach {
-                    val imageView = ImageView(this@FontsActivity)
-                    linearLayoutCoroutines.addView(
-                            imageView
-                    )
-                    val job = fonts.fontDeferred(it)
-                    coroutines.add(job)
-
-                    launch(UI) {
-                        imageView.setImageBitmap(job.await())
-                    }
+                runnable?.let {
+                    linearLayoutCoroutines.handler.removeCallbacks(it)
                 }
+
+                runnable = Runnable({ coroutineTextChanged(text) })
+                linearLayoutCoroutines.handler.postDelayed(runnable, 500);
             }
-
         })
+    }
 
-        compositeTextChange.add(RxTextView.afterTextChangeEvents(editTextName)
-                .filter { it.editable().toString().isNotEmpty() }
-                .map { it.editable().toString() }
-                .doOnNext {
-                    linearLayoutRx.removeAllViews()
-                    compositeFonts.clear()
-                }.flatMap { Observable.fromIterable(it.asIterable()) }
-                .doOnNext {
-                    val imageView = ImageView(FontsActivity@this)
-                    linearLayoutRx.addView(
-                            imageView
-                    )
+    private fun coroutineTextChanged(text: String) {
+        resetViews(linearLayoutCoroutines, text.length)
+        cancelPendingCoroutines()
 
-                    compositeFonts.add(fonts.font(it).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe {
-                        imageView.setImageBitmap(it)
-                    })
+        if (text.isEmpty())
+            if (text.isEmpty())
+                return
 
-                }
-                .subscribe())
+        text.toCharArray().forEachIndexed { index, char ->
+            val job = fonts.fontDeferred(char)
+            coroutines.add(job)
 
+            launch(UI) {
+                val imageView = linearLayoutCoroutines.getChildAt(index) as ImageView
+                imageView.setImageBitmap(job.await())
+                imageView.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    private fun cancelPendingCoroutines() {
+        coroutines.forEach { it.cancel() }
+    }
+
+    private fun resetViews(layout: LinearLayout, length: Int) {
+        for (i in 1 until length) {
+            (layout.getChildAt(i) as ImageView).setImageBitmap(null)
+        }
+        for (i in length..24) {
+            layout.getChildAt(i).visibility = View.GONE
+        }
     }
 
     override fun onPause() {
         super.onPause()
+        disposeRx()
+
+        cancelPendingCoroutines()
+    }
+
+    private fun disposeRx() {
         compositeTextChange.clear()
+        compositeFonts.clear()
     }
 }
